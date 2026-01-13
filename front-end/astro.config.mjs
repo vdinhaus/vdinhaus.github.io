c364a0ea43b6a328542bdbfd0dbf2892315a908d
vdinhaus <vd11.imagine@gmail.com>
2026-01-13 14:15:19 +1100

    Set base path for front-end configuration

--- front-end/astro.config.mjs
+++ front-end/astro.config.mjs
@@ -8,5 +8,6 @@ const isProd = import.meta.env.MODE === "production";
 // https://astro.build/config
 export default defineConfig({
   site: isProd ? "https://vdinhaus.github.io" : "http://localhost:4321",
+  base: "/front-end",
   integrations: [react()],
 });
