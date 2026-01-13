061c78335406847ad46d1cec208d7db1700affab
vdinhaus <vd11.imagine@gmail.com>
2026-01-13 14:05:55 +1100

    Recreated Repo

---
+++ README.md
@@ -0,0 +1,2 @@
+Welcome to my portfolio, built to run directly within GitHub using GitHub Sites. It covers a wide range of topics, from design and development, to photography
+and art!
---
+++ front-end/astro.config.mjs
@@ -0,0 +1,12 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+import react from "@astrojs/react";
+
+const isProd = import.meta.env.MODE === "production";
+
+// https://astro.build/config
+export default defineConfig({
+  site: isProd ? "https://vdinhaus.github.io" : "http://localhost:4321",
+  integrations: [react()],
+});
